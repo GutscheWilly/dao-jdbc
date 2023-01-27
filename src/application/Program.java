c9308216fad7b4e22b3b8091995e4d76794428b1
Willy Gutsche <willy.gutsche@unifesp.br>
2023-01-27 19:55:03 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) throws Exception {
         Department department = new Department(1, "RH");
-        System.out.println(department);
+        Seller seller = new Seller(1, "Willy", "willygutsche@gmail.com", new Date(), 3000.0, department);
+        System.out.println(seller);
     }
 }
