8131c35ad141f333a70d25c9be37accec01b7d93
Willy Gutsche <willy.gutsche@unifesp.br>
2023-01-28 04:34:01 -0300

    SellerDaoJDBC - method: Seller findById()

--- src/dao/contracts/SellerDaoJDBC.java
+++ src/dao/contracts/SellerDaoJDBC.java
@@ -1,11 +1,25 @@
 package dao.contracts;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 import dao.Dao;
+import database.Database;
+import database.DatabaseException;
+import entities.Department;
 import entities.Seller;
 
 public class SellerDaoJDBC implements Dao<Seller> {
+
+    private Connection connection;
+
+    public SellerDaoJDBC(Connection connection) {
+        this.connection = connection;
+    }
 
     @Override
     public void insert(Seller seller) {
@@ -24,7 +38,50 @@ public class SellerDaoJDBC implements Dao<Seller> {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(
+                "SELECT seller.*, department.Name as DepartmentName " +
+                "FROM seller INNER JOIN department "                  +
+                "ON seller.DepartmentId = department.Id "             +
+                "WHERE seller.Id = ?"
+            );
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return createSeller(resultSet);
+            }
+            return null;
+        }
+        catch (SQLException sqlException) {
+            throw new DatabaseException(sqlException.getMessage());
+        }
+        finally {
+            Database.closeResultSet(resultSet);
+            Database.closeStatement(preparedStatement);
+        }
+    }
+
+    private Seller createSeller(ResultSet resultSet) {
+        try {
+            Integer id = resultSet.getInt("Id");
+            String name = resultSet.getString("Name");
+            String email = resultSet.getString("Email");
+            Date birthDate = resultSet.getDate("BirthDate");
+            Double baseSalary = resultSet.getDouble("BaseSalary");
+
+            Integer departmentId = resultSet.getInt("DepartmentId");
+            String departmentName = resultSet.getString("DepartmentName");
+            Department department = new Department(departmentId, departmentName);
+
+            return new Seller(id, name, email, birthDate, baseSalary, department);
+        } 
+        catch (SQLException sqlException) {
+            throw new DatabaseException("Error to create seller!");
+        }
     }
 
     @Override
