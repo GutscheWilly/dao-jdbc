91e98c5a33a7cce40bd709ffde16648f882c21d8
Willy Gutsche <willy.gutsche@unifesp.br>
2023-02-01 18:49:58 -0300

    DepartmentDaoJDBC - method: findAll

--- src/application/TestDepartmentDaoJDBC.java
+++ src/application/TestDepartmentDaoJDBC.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import dao.Dao;
 import dao.DaoFactory;
 import entities.Department;
@@ -9,12 +11,20 @@ public class TestDepartmentDaoJDBC {
     public static void main(String[] args) {
         Dao<Department> departmentDao = DaoFactory.createDepartmentDao();
         testFindById(departmentDao, 4);
+        testFindAll(departmentDao);
     }
 
     public static void testFindById(Dao<Department> departmentDao, Integer id) {
         System.out.println("TEST 1: findById");
         Department department = departmentDao.findById(id);
         System.out.println(department);
+        System.out.println("-------------------------------------------------------------------------------------");
+    }
+
+    public static void testFindAll(Dao<Department> departmentDao) {
+        System.out.println("TEST 2: findAll");
+        List<Department> listOfDepartments = departmentDao.findAll();
+        listOfDepartments.stream().forEach(System.out::println);
         System.out.println("-------------------------------------------------------------------------------------");
     }
 }
