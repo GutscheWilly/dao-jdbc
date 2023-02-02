676955672f6e05896de88b95cbd43142bcc3e0ce
Willy Gutsche <willy.gutsche@unifesp.br>
2023-02-02 19:30:47 -0300

    DepartmentDaoJDBC - menu option

--- src/application/TestDepartmentDaoJDBC.java
+++ src/application/TestDepartmentDaoJDBC.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 import dao.Dao;
 import dao.DaoFactory;
+import database.DatabaseException;
 import entities.Department;
 
 public class TestDepartmentDaoJDBC {
@@ -12,11 +14,50 @@ public class TestDepartmentDaoJDBC {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Dao<Department> departmentDao = DaoFactory.createDepartmentDao();
-        testFindById(departmentDao, 4);
-        testFindAll(departmentDao);
-        testInsert(departmentDao, scanner);
-        testUpdate(departmentDao, 5, scanner);
-        testDeleteById(departmentDao, 6);
+        boolean runningProgram = true;
+
+        while (runningProgram) {
+            try {
+                switch (printMenuOptions(scanner)) {
+                    case 1:
+                        System.out.print("Enter a ID: ");
+                        testFindById(departmentDao, scanner.nextInt());
+                        break;
+                    case 2:
+                        testFindAll(departmentDao);
+                        break;
+                    case 3:
+                        testInsert(departmentDao, scanner);
+                        break;
+                    case 4:
+                        System.out.print("Enter a ID: ");
+                        testUpdate(departmentDao, scanner.nextInt(), scanner);
+                        break;
+                    case 5:
+                        System.out.print("Enter a ID: ");
+                        testDeleteById(departmentDao, scanner.nextInt());
+                        break;
+                    case 0:
+                        runningProgram = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option!");
+                }
+            }
+            catch (InputMismatchException inputMismatchException) {
+                clearScreen();
+                System.out.println("Input Error!\n");
+                scanner.nextLine();
+            }
+            catch (DatabaseException databaseException) {
+                clearScreen();
+                System.out.println("Database Error! " + databaseException.getMessage() + "\n");
+                scanner.nextLine();
+            }
+            catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 
     public static void testFindById(Dao<Department> departmentDao, Integer id) {
@@ -63,5 +104,22 @@ public class TestDepartmentDaoJDBC {
         departmentDao.deleteById(id);
         System.out.println("Department deleted: " + department);
         System.out.println("-------------------------------------------------------------------------------------");
+    }
+
+    public static Integer printMenuOptions(Scanner scanner) {
+        System.out.println("Department Data Acess Object");
+        System.out.println("1 - Find by ID");
+        System.out.println("2 - Find all");
+        System.out.println("3 - Insert");
+        System.out.println("4 - Update by ID");
+        System.out.println("5 - Delete by ID");
+        System.out.println("0 - Close program");
+        System.out.print("Enter an option: ");
+        return scanner.nextInt();
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
